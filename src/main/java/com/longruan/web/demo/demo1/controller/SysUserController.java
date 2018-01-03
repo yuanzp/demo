317f138902de74ce86879e9bc8167ff9b3ecd9e4
mosty <mostyuan@hotmail.com>
2018-01-03 22:54:17 +0800

    test2

--- src/main/java/com/longruan/web/demo/demo1/controller/SysUserController.java
+++ src/main/java/com/longruan/web/demo/demo1/controller/SysUserController.java
@@ -1,6 +1,11 @@
 package com.longruan.web.demo.demo1.controller;
 
 
+import com.longruan.web.demo.demo1.service.SysUserService;
+import com.longruan.web.demo.demo1.utils.ResultGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @Validated
 public class SysUserController {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private final SysUserService userService;
+
+    private final ResultGenerator generator;
+
+    @Autowired  //自动装配
+    public SysUserController(SysUserService userService, ResultGenerator generator) {
+        this.userService = userService;
+        this.generator = generator;
+    }
 
 }
